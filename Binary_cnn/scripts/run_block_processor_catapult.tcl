999d6c5db847d2be1b58ded56c81c29d0a023e03
MYEONGJINLEE <mjlee7466@google.com>
2026-02-05 17:45:30 +0900

    Trim block processor scripts to cpp and tb

--- Binary_cnn/scripts/run_block_processor_catapult.tcl
+++ Binary_cnn/scripts/run_block_processor_catapult.tcl
@@ -14,12 +14,6 @@ solution options set /Flows/Enable-SCVerify yes
 solution options set /Output/GenerateCycleNetlist false
 
 # Design + testbench files
-solution file add ./src/streaming/bw_cnn_streaming.h -type C++
-solution file add ./src/streaming/block_config.h -type C++
-solution file add ./src/streaming/tile_manager.h -type C++
-solution file add ./src/streaming/inter_layer_buffer.h -type C++
-solution file add ./src/streaming/conv_compute.h -type C++
-solution file add ./src/streaming/block_processor.h -type C++
 solution file add ./src/streaming/block_processor.cpp -type C++
 solution file add ./src/streaming/block_processor_tb.cpp -type C++ -exclude true
 
